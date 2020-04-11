48b2a61b0784fe66759df082ed756a4e8e85d75c
ishidafuu <ishidafuu@gmail.com>
2020-04-11 19:15:31 +0900

    update

--- functions/node/index.dart
+++ functions/node/index.dart
@@ -2,9 +2,15 @@ import 'package:firebase_functions_interop/firebase_functions_interop.dart';
 
 void main() {
   functions['helloWorld'] = functions.https.onRequest(helloWorld);
+  functions['hellWorld'] = functions.https.onRequest(hellWorld);
 }
 
 void helloWorld(ExpressHttpRequest request) {
   request.response.writeln('Hello world');
+  request.response.close();
+}
+
+void hellWorld(ExpressHttpRequest request) {
+  request.response.writeln('Hell world...');
   request.response.close();
 }
